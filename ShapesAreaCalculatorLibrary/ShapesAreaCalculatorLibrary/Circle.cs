0686d6e043c54b3f324e6dc6db1f7bfa811d0b64
Alex Newman <bakaev_a1998@mail.ru>
2023-03-24 15:03:55 +0600

    Added circle class

---
+++ ShapesAreaCalculatorLibrary/ShapesAreaCalculatorLibrary/Circle.cs
@@ -0,0 +1,34 @@
+﻿using ShapesAreaCalculatorLibrary.Interfaces;
+using System;
+
+namespace ShapesAreaCalculatorLibrary
+{
+    /// <summary>
+    /// Circle shape
+    /// </summary>
+    public class Circle : IShape
+    {
+        /// <summary>
+        /// Circle radius
+        /// </summary>
+        public double Radius { get; }
+
+        /// <summary>
+        /// Initializes class instance of <see cref="Circle"/>
+        /// </summary>
+        /// <param name="radius">Circle radius</param>
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// Gets area of the circle by its radius
+        /// </summary>
+        /// <returns>Area of the circle</returns>
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
