599c97b10b56d3b4ed7a4bade01d1024573a2bad
Alex Newman <bakaev_a1998@mail.ru>
2023-03-24 14:21:37 +0600

    Added IShape interface

---
+++ ShapesAreaCalculatorLibrary/ShapesAreaCalculatorLibrary/Interfaces/IShape.cs
@@ -0,0 +1,14 @@
+﻿namespace ShapesAreaCalculatorLibrary.Interfaces
+{
+    /// <summary>
+    /// Describes the shape interface
+    /// </summary>
+    public interface IShape
+    {
+        /// <summary>
+        /// Gets an area of a shape
+        /// </summary>
+        /// <returns>Area of the shape</returns>
+        public double GetArea();
+    }
+}
