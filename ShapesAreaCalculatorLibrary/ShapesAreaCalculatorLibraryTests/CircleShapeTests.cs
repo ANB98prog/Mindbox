dceb129d32e92d9d9a83c97a691a46474246f680
Alex Newman <bakaev_a1998@mail.ru>
2023-03-24 15:04:11 +0600

    Added tests for circle

--- ShapesAreaCalculatorLibrary/ShapesAreaCalculatorLibraryTests/CircleShapeTests.cs
+++ ShapesAreaCalculatorLibrary/ShapesAreaCalculatorLibraryTests/CircleShapeTests.cs
@@ -1,11 +1,46 @@
+using ShapesAreaCalculatorLibrary;
+using ShapesAreaCalculatorLibrary.Interfaces;
+
 namespace ShapesAreaCalculatorLibraryTests
 {
     public class CircleShapeTests
     {
         [Fact]
-        public void Test1()
+        public void CreateCircleFact()
         {
+            double radius = 5;
 
+            Circle circle = new Circle(radius);
+
+            Assert.NotNull(circle);
+            Assert.Equal(radius, circle.Radius);
+        }
+
+        [Fact]
+        public void CreateCircleByInterfaceFact()
+        {
+            double radius = 5;
+
+            IShape circle = new Circle(radius);
+
+            Assert.NotNull(circle);
+        }
+
+        [Fact]
+        public void CreateCircleWithNegativeRadiusFact()
+        {
+            double radius = -5;
+
+            Assert.Throws<ArgumentException>(() => new Circle(radius));
+        }
+
+        [Theory]
+        [InlineData(1, 3.14)]
+        public void GetCircleAreaFact(double radius, double expected)
+        {
+            Circle circle  = new Circle(radius);
+
+            Assert.Equal(expected, circle.GetArea());
         }
     }
 }
