345b03910ce1b8400a65343e1fcd7b765b7c7ddb
Alex Newman <bakaev_a1998@mail.ru>
2023-03-24 16:03:27 +0600

    Added triangle shape

---
+++ ShapesAreaCalculatorLibrary/ShapesAreaCalculatorLibrary/Shapes/Triangle.cs
@@ -0,0 +1,68 @@
+﻿using ShapesAreaCalculatorLibrary.Interfaces;
+using System;
+
+namespace ShapesAreaCalculatorLibrary.Shapes
+{
+    /// <summary>
+    /// Triangle shape
+    /// </summary>
+    public class Triangle : IShape
+    {
+        /// <summary>
+        /// Triangle side 'a'
+        /// </summary>
+        public double A { get; private set; }
+
+        /// <summary>
+        /// Triangle side 'b'
+        /// </summary>
+        public double B { get; private set; }
+
+        /// <summary>
+        /// Triangle side 'c'
+        /// </summary>
+        public double C { get; private set; }
+
+        /// <summary>
+        /// Initializes class instance of <see cref="Triangle"/>
+        /// </summary>
+        /// <param name="a">Triangle side 'a'</param>
+        /// <param name="b">Triangle side 'b'</param>
+        /// <param name="c">Triangle side 'c'</param>
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        /// <summary>
+        /// Whether triangle is right-angled
+        /// </summary>
+        /// <returns>true if it is else if not</returns>
+        public bool IsRightAngled()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets area of triangle by its three sides
+        /// </summary>
+        /// <returns>Area of the triangle</returns>
+        public double GetArea()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        /// <summary>
+        /// Gets area of triangle by its three sides
+        /// </summary>
+        /// <param name="precision">Result precision</param>
+        /// <returns>Area of the triangle with specified precision</returns>
+        public double GetArea(int precision)
+        {
+            return Math.Round(GetArea(), precision);
+        }
+    }
+}
